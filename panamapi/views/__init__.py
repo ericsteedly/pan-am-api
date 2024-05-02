476b3024acf7a128c00f085707115e59cac8b392
Eric Steedly <ericsteedly@gmail.com>
2024-05-02 14:15:16 -0500

    created/registered airport viewset for list support

--- panamapi/views/__init__.py
+++ panamapi/views/__init__.py
@@ -1,2 +1,3 @@
 from .register import login_user, register_user
 from .flight import Flights
+from .airport import Airports
--- panamapi/views/airport.py
+++ panamapi/views/airport.py
@@ -1,5 +1,14 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from panamapi.models import Airport
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from panamapi.models import Airport
+
+class Airports(ViewSet):
+    def list(self, request):
+        airports = Airport.objects.all()
+        serialized = AirportSerializer(airports, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
 
 class AirportSerializer(serializers.ModelSerializer):
 
