a7668bbf7f5f697082298a5fdfb690cd5a175350
Eric Steedly <ericsteedly@gmail.com>
2024-05-22 18:10:47 -0500

    added frontend url to cors whitelist, installed deployment packages

--- panam/settings.py
+++ panam/settings.py
@@ -72,7 +72,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ("http://localhost:3000", "http://127.0.0.1:3000")
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "pan-am-eight.vercel.app"
+      )
 
 CSRF_TRUSTED_ORIGINS =['http://localhost:3000']
 
