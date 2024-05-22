71422cdbac354d19183fb2be645449cd0e561343
Eric Steedly <ericsteedly@gmail.com>
2024-05-22 17:43:39 -0500

    updated settings.py for digital ocean deployment, created .env file

--- panam/settings.py
+++ panam/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.core.management.utils import get_random_secret_key
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,13 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3d(h38f=km(l0@ofhm+9d5waedhzm5=1(yptp(q2fc6zlgy1nb'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", '').split(",")
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False")
 
 # Application definition
 
