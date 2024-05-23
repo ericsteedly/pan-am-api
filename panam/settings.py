5c74bc76920426f58c1aeb070c7acb8cb3f03652
Eric Steedly <ericsteedly@gmail.com>
2024-05-23 11:05:30 -0500

    added cors_allowed_headers

--- panam/settings.py
+++ panam/settings.py
@@ -90,6 +90,18 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'panam.urls'
 
 TEMPLATES = [
